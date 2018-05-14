88087984c518e4da699e0820bf7594ea6c6fbadf
Sateesh Kodati <sateesh.kodati@gmail.com>
2018-05-14 01:42:16 -0700

    ui app updated bundle

--- web/src/components/ProductList.jsx
+++ web/src/components/ProductList.jsx
@@ -40,21 +40,7 @@ function ProductList(props) {
                 )}
                 </tbody>
             </table>
-            <nav aria-label="...">
-                <ul className="pagination">
-                    <li className="page-item disabled">
-                        <a className="page-link" href="#" tabIndex="-1">Previous</a>
-                    </li>
-                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item active">
-                        <a className="page-link" href="#">2 <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    <li className="page-item">
-                        <a className="page-link" href="#">Next</a>
-                    </li>
-                </ul>
-            </nav>
+            
             { showProduct && 
             <Product
             showProduct={showProduct}
